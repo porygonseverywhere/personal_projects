bedb947a60ed8fedc5cbaf56f209e33a90fcaa4b
porygonseverywhere <143299485+porygonseverywhere@users.noreply.github.com>
2024-01-23 20:30:22 -0500

    Update poke_builder.py

--- pocket_monsters/poke_builder.py
+++ pocket_monsters/poke_builder.py
@@ -1,4 +1,7 @@
 
+"""
+dictionary of common api codes and responses helpful for debugging
+"""
 global API_DICT
 API_DICT = {
     200: {'status': 'OK', 
@@ -26,7 +29,10 @@ API_DICT = {
 global BASE_URL
 BASE_URL = "http://pokeapi.co/api/v2"
 
-# api endpoints of interest from api
+
+"""
+more can be appended in the future. these contain the various endpoints for the api url's
+"""
 global ENDPOINTS
 ENDPOINTS = [
     "ability",
@@ -45,62 +51,75 @@ import requests
 import json
 import pandas as pd 
 import os
+import numpy as np
+import postgre_connect as sql
 
 
 
 
-class Connect_to_API:
-
-    def get_data(self, api):
-        """
-        Pull root api data that allows you to make connections to other dataframes.
-        """
-        response = requests.get(f"{api}")
-        if response.status_code == 200:
-            print("Sucessfully fetched the data.")
-            self.data = self.root_json_to_df(response.json())
-        elif response.status_code in API_DICT.keys():
-            print(API_DICT[response.status_code])
-        else:
-            print("Something went wrong. The api status code was not found in our dictionary.")
-
-
-
-    def root_json_to_df(self, obj):
-        text = json.dumps(obj, sort_keys=True, indent=4)
-        data = pd.json_normalize(obj)
-        return data
-
-
-    def __init__(self, api):
-        self.get_data(api)
-        self.data = pd.DataFrame()
-        
-
-    # def __call__(self):
-    #     return self.data
-
-
-
-def api_url(base, endpoint):
+def build_url(base, endpoint):
     return '{}/{}'.format(base, endpoint)
+
+
+def connect_to_api(api):
+    if 'api' not in locals():
+        raise Exception("No api url found in connect_to_api(). Stopping the script.")
+
+    response = requests.get(f"{api}")
+
+    if response.status_code == 200:
+        print("Sucessfully fetched the data.")
+
+    elif response.status_code in API_DICT.keys():
+        print(API_DICT[response.status_code])
+    else:
+        print("Something went wrong. The api status code was not found in our dictionary.")
+
+    return response
+
+def root_json_to_df(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    #print(text)
+    df = pd.DataFrame.from_dict(obj['results'])
+    """
+    api calls seem to have the same structure.
+    so str[-2] grabs the id
+    replace the "name" w/ the str[-3]?
+    """
+    df['id'] = df['url'].str.split('/').str[-2]
+    df.rename(columns = {'name' : df['url'].str.split('/').str[-3].iloc[0]}, inplace = True)
+    df = df.drop(columns = ['url'])
+
+    return df
+
 
 def main():
 
+    connection = sql.test_connection()
+
+    combined_poke_data = {}
+
     for x in ENDPOINTS:
-        api = Connect_to_API(api_url(BASE_URL, x))
-        print(api.data)
+        api_call = connect_to_api(build_url(BASE_URL, x))
+
+        combined_poke_data["df_{}".format(x)] = root_json_to_df(api_call.json())
+        # You can access the dataframes by call combined_poke_data[key], where key = "df_endpoint_1", "df_endpoint_2", ...
+        # create check for hyphens and replace with underscores.
+        tmp = combined_poke_data["df_{}".format(x)]
+        tmp.columns = tmp.columns.str.replace("-", "_", regex=True)
+        
+        col = sql.create(connection, tmp)
+        sql.insert(connection, tmp, col)
+
+    connection.close()
+
 
 
 
 
 if __name__ == "__main__":
+
+    #not only do we want to be able to send data to and read from a storage solution
+    #but we should have a check that scans the api site to make sure there aren't any updates
+    #if there are, repull the data to get the latest.
     main()
-
-#makes the api calls but how do we return the df and make it accessible?
-
-
-# present a list of Game Versions
-# with the input of a game version, return pokemon, type, damage relationships
-
-# database that contains pokemon table 1, game version table 2, generation? table 3, damage relationship{double dmg from, double dmg to, half damage from, non damg to, etc.}
